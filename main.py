2f25726b398c886c08a60bbb42c25728422fc57d
ODFA <35944090+ODFA@users.noreply.github.com>
2018-02-08 13:48:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,24 @@
 import numpy
 import random
-#Setting the amount of nodes in each layer
-inputNodes = 100 #Place holder
-hiddenNodes = 50
-outputNodes = 10
-#Setting how many training examples the network should go through
-trainingIterations = int(input("How many training iterations?"))
-#Creating a sigmoid function to keep neuron values between 0 and 1, and so you can find the derivative of a poing
+#Setting the number of nodes for each layer
+inputNodes = 5
+hiddenNodes = 5
+outputNodes = 2
+#Creating a sigmoid function to keep neuron values between 0 and 1, and so you can find the derivative of a point
 def sigmoid(vector, derivativeMode = False):
-	if derivativeMode: return (x * (x - 1)) #TODO Ask Elgin how this finds the derivative
-	#Return a number to keep the neuron values between 0 and 1
-	else: return (1 / (1 + numpy.exp(-x))
-#TODO talk to Elgin about training data for digits
-#Creating weights
-#Will go unused for now, so I can allow myself to start with a simpler network
-inputWeights = [random.uniform(-1, 1) for m in xrange(inputNodes)]
-hiddenWeights = [random.uniform(-1, 1) for n in xrange(hiddenNodes)]
-outputWeights = [random.uniform(-1, 1) for o in xrange(outputNodes)]
+	if derivativeMode: return (x * (x - 1)) #Return the derivative if in derivative mode
+	else: return (1 / (1 + numpy.exp(-x)) #Return sigmoid(x) if not in derivative mode
+#Creating a function that gives you the numpy array for an example
+def ideal(inputValue):
+	if inputValue[0] == 1 and inputValue[-1] == 1: return numpy.array([0, 1])
+	else: return numpy.array([1, 0])
 #Creating the synapses
-#I will use my own for loop system for now, so I can understand it better
-inputSynapses = np.array([[random.uniform(-1, 1) for i in xrange(hiddenNodes)] for j in xrange(inputNodes)]) #Is np.array needed?
-hiddenSynapses = np.array([[random.uniform(-1, 1) for k in xrange(outputNodes)] for l in xrange(hiddenNodes)]) #"
-#Training loop WIP
-for p in xrange(trainingIterations):
-	inputValues = null #Place holder
-	hiddenValues = null
-	OutputVales = null
-	#TODO Back propogation
-
-while (True):
-	inputValues = null #Place holder
-	outputValues =
-	for q in outputValues:
-		largest = 0
-		if (q > largest):
-			largest = q
-	print(outputValues.index(largest))
+inputSynapses = np.random.random((inputNodes, hiddenNodes)) * 2 - 1 #Create an array with inputNodes elements that contain arrays with hiddenNodes elements that contain a value between -1 and 1
+hiddenSynapses = np.random.random((hiddenNodes, outputNodes)) * 2 - 1 #Create an array with hiddenNodes elements that contain arrays with outputNodes elements that contain a value between -1 and 1
+#Training loop
+for p in xrange(int(raw_input("How many training iterations?"))):
+	#Foward propegation
+	inputValues = [random.randint(0, 2) for i in xrange(0, 6)] #Create a random data sample
+	hiddenValues = sigmoid(numpy.dot(inputValues, inputSynapses)) #Find the dot product of inputValues and inputSynapses, and put the list through the sigmoid function to keep Node values between 0 and 1
+	outputValues = sigmoid(numpy.dot(hiddenValues, hiddenSynapses)) #Find the dot product of outputValues and hiddenSynapses, and put the list through the sigmoid function to keep the values between 0 and 1
+	idealValues = sigmoid(ideal(inputValues))
