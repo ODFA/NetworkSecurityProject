351bd4455f73c342d304f75f9216c02006f44690
ODFA <ofrenstd@arcadiacharterschool.org>
2018-03-03 22:20:28 -0600

    Code is now more organized, but the bug is still there.

--- main.py
+++ main.py
@@ -1,42 +1,54 @@
 import numpy
 import random
-#Setting the number of nodes for each layer
-inputNodes = 5
-hiddenNodes = 5
-outputNodes = 1
-trainingItterations = 10000
+
+#Initializing hyperparamaters
+layerOneNodes = 5
+layerTwoNodes = 5
+layerThreeNodes = 1
+
 #Creating a sigmoid function to keep neuron values between 0 and 1, and so you can find the derivative of a point
 def sigmoid(vector, derivativeMode = False):
 	if derivativeMode: return (vector * (vector - 1)) #Return the derivative if in derivative mode
 	else: return (1 / (1 + numpy.exp(-vector))) #Return sigmoid(x) if not in derivative mode
-def ideal(values):
-	if (values[0] == 1 and values[-1] == 1): return True
-	else: return False
+
+def getLabel(values):
+	if (values[0] == 1 and values[-1] == 1): return 1 #If the first and last values of values is 1, return True
+	else: return 0
+
 #Creating random values for training data
-trainingValues = numpy.array([[random.getrandbits(1) for i in xrange(inputNodes)] for j in xrange(pow(2, inputNodes))])
-idealOutputs = numpy.array([[ideal(sample) for sample in trainingValues]]).T
+trainingValues = numpy.array([[random.getrandbits(1) for i in xrange(layerOneNodes)] for j in xrange(pow(2, layerOneNodes))]) #Creating
+idealOutputs = numpy.array([[getLabel(sample) for sample in trainingValues]]).T
+
 #Creating random synapses
 numpy.random.seed(1)
-inputSynapses = numpy.random.random((inputNodes, hiddenNodes)) * 2 - 1 #Create an array with inputNodes elements that contain arrays with hiddenNodes elements that contain a value between -1 and 1
-hiddenSynapses = numpy.random.random((hiddenNodes, outputNodes)) * 2 - 1 #Create an array with hiddenNodes elements that contain arrays with outputNodes elements that contain a value between -1 and 1
+LayerOneSynapses = (numpy.random.random((layerOneNodes, layerTwoNodes)) * 2) - 1 #Create an array with layerOneNodes elements that contain arrays with layerTwoNodes elements that contain a value between -1 and 1
+LayerTwoSynapses = (numpy.random.random((layerTwoNodes, layerThreeNodes)) * 2) - 1 #Create an array with layerTwoNodes elements that contain arrays with layerThreeNodes elements that contain a value between -1 and 1
+
 #Training loop
-print(hiddenSynapses)
-for p in range(trainingItterations):
+print(LayerOneSynapses)
+for p in xrange(int(raw_input("Training iterations: "))):
+
 	#Foward propegation
 	inputValues = trainingValues
-	hiddenValues = sigmoid(numpy.dot(inputValues, inputSynapses)) #Find the dot product of inputValues and inputSynapses, and put the list through the sigmoid function to keep Node values between 0 and 1
-	outputValues = sigmoid(numpy.dot(hiddenValues, hiddenSynapses)) #Find the dot product of outputValues and hiddenSynapses, and put the list through the sigmoid function to keep the values between 0 and 1
+	hiddenValues = sigmoid(numpy.dot(inputValues, LayerOneSynapses)) #Find the dot product of inputValues and LayerOneSynapses, and put the list through the sigmoid function to keep Node values between 0 and 1
+	outputValues = sigmoid(numpy.dot(hiddenValues, LayerTwoSynapses)) #Find the dot product of outputValues and LayerTwoSynapses, and put the list through the sigmoid function to keep the values between 0 and 1
+
 	#Back propegation
 	outputError = idealOutputs - outputValues
 	outputDelta = outputError * sigmoid(outputValues, derivativeMode = True)
-	hiddenError = numpy.dot(outputDelta, hiddenSynapses.T)
+	hiddenError = numpy.dot(outputDelta, LayerTwoSynapses.T)
 	hiddenDelta = hiddenError * sigmoid(hiddenValues, derivativeMode = True)
-	inputSynapses += numpy.dot(inputValues.T, hiddenDelta)
-	hiddenSynapses += numpy.dot(hiddenValues.T, outputDelta)
-print(hiddenSynapses)
+
+	LayerOneSynapses += numpy.dot(inputValues.T, hiddenDelta)
+	LayerTwoSynapses += numpy.dot(hiddenValues.T, outputDelta)
+
+print(LayerOneSynapses) #TEMP
+
 while (True):
-	inputValues = numpy.array([int(raw_input("0 or 1\n")) for r in xrange(0, 5)])
-	hiddenValues = sigmoid(numpy.dot(inputValues, inputSynapses))
-	outputValues = sigmoid(numpy.dot(hiddenValues, hiddenSynapses))
-	print(outputValues)
+
+	inputValues = numpy.array([int(raw_input("0 or 1\n")) for r in xrange(LayerOneSynapses)])
+	hiddenValues = sigmoid(numpy.dot(inputValues, LayerOneSynapses))
+	outputValues = sigmoid(numpy.dot(hiddenValues, LayerTwoSynapses))
+
+	print(outputValues) #TEMP
 	print("Network Predicts: " + str(int(round(outputValues))))
